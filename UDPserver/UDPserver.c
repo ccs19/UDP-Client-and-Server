ca5373f93679fbb29e57a9e473b460ce6a6a25e9
christopher <ccs19@students.uwf.edu>
2015-02-03 13:17:23 -0600

    Copy and pasted the TCP server Changed socket type to datagram.

--- UDPserver/UDPserver.c
+++ UDPserver/UDPserver.c
@@ -2,12 +2,46 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <netdb.h> //For gethostbyname()
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h> //for memset
 
 #include "UDPserver.h"
 
 
-int main()
+//Globals
+int ServerSocket = 0;
+struct hostent *HostByName = NULL;
+struct sockaddr_in ServerAddress;
+
+//Constants
+const int HostNameMaxSize = 256;
+const int MAXLISTENERS = 5;
+const int BUFFERSIZE = 256;
+const int PORT_UNDEFINED = 99999;
+const int MAX_PORT = 65535;
+
+int main(int argc, const char* argv[])
 {
+    unsigned int port = PORT_UNDEFINED;
+
+    if(argc > 2)
+        printf("Usage:\n%s <optional:port to open>\n",argv[0]);
+    else if(argc == 1) //If no port specified
+        port = 0;
+    else if(atoi(argv[1]) <= 0 || atoi(argv[1]) > MAX_PORT) //Checking valid port bounds
+        printf("Invalid port number\n");
+    else {
+        if (port == PORT_UNDEFINED) port = atoi(argv[1]); //If port specified at command line
+    }
+
+    if(port != PORT_UNDEFINED)
+    {
+        OpenSocket(port); //Open socket with port in arg vector 1
+        AcceptConnections();
+    }
     return 0;
 }
 
@@ -20,7 +54,21 @@ int main()
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 void OpenSocket(int port)
 {
+    char hostname[HostNameMaxSize];
 
+    if( ( ServerSocket =  socket(PF_INET, SOCK_DGRAM, 0) ) <  0)  //If socket fails
+        ExitOnError("Error creating socket");
+
+    if(gethostname(hostname, sizeof(hostname)) < 0)                         //If getting hostname fails
+        ExitOnError("Error acquiring hostname. Exiting");
+
+    if( ( HostByName = gethostbyname(hostname) ) ==  NULL)                  //If gethostbyname fails print error message, exit
+    {
+        herror("GetHostByName failed. Error: ");
+        exit(1);
+    }
+    InitAddressStruct(port);
+    BindSocketAndListen();
 }
 
 
@@ -33,7 +81,10 @@ void OpenSocket(int port)
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 void InitAddressStruct(int port)
 {
-
+    memset((void*)&ServerAddress, '0', (size_t)sizeof(ServerAddress));
+    ServerAddress.sin_family = AF_INET;
+    memcpy( (void *)&ServerAddress.sin_addr, (void *)HostByName->h_addr, HostByName->h_length);
+    ServerAddress.sin_port = htons(port);
 }
 
 
@@ -45,7 +96,20 @@ void InitAddressStruct(int port)
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 void DisplayInfo()
 {
-
+    int i = 0;
+    struct in_addr ipAddress;
+    struct sockaddr_in sockIn;
+    int sockLen = sizeof(sockLen);
+    getsockname(ServerSocket, (struct sockaddr*)&sockIn, (socklen_t*)&sockLen );
+    printf("Host Name: %s\n",HostByName->h_name);
+    printf("IP:        ");
+    while(HostByName->h_addr_list[i] != 0)
+    {
+        ipAddress.s_addr = *(u_long*)HostByName->h_addr_list[i++];
+        printf("%s\n", inet_ntoa(ipAddress));
+    }
+    printf("Port:      %d\n", htons(sockIn.sin_port));
+    fflush(stdout);
 }
 
 
@@ -57,7 +121,10 @@ void DisplayInfo()
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 void BindSocketAndListen()
 {
-
+    if( ( bind( ServerSocket, (struct sockaddr *) &ServerAddress, sizeof(ServerAddress)) )  < 0)
+        ExitOnError("Failed to bind socket"); //If binding of socket fails
+    if( (listen( ServerSocket, MAXLISTENERS)) < 0)
+        ExitOnError("Failed to listen");
 }
 
 
@@ -69,7 +136,26 @@ void BindSocketAndListen()
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 void AcceptConnections()
 {
+    /*~~~~~~~~~~~~~~~~~~~~~Local vars~~~~~~~~~~~~~~~~~~~~~*/
+    struct sockaddr_in ClientAddress;
+    unsigned int clientAddressSize = sizeof(ClientAddress);
+    pthread_attr_t ThreadAttribute;
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
+    DisplayInfo();
+    pthread_attr_init(&ThreadAttribute);
+    pthread_attr_setdetachstate(&ThreadAttribute, PTHREAD_CREATE_DETACHED);
+    printf("Waiting for connection... ");
+    fflush(stdout);
+    for(;;)
+    {
+        pthread_t DetachedThread;
+        int *ClientSocket = malloc(sizeof(int));
+
+        if( (*ClientSocket = accept(ServerSocket, (struct sockaddr*)&ClientAddress, &clientAddressSize) ) < 0)
+            ExitOnError("Error in accept()");
+        pthread_create(&DetachedThread, &ThreadAttribute, (void*)HandleClientRequests, (void*)ClientSocket);
+    }
 }
 
 
@@ -97,7 +183,27 @@ void ExitOnError(char* errorMessage)
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 void HandleClientRequests(void* ClientSocketPtr)
 {
-
+    printf("Client connected!\n");
+    fflush(stdout);
+    /*~~~~~~~~~~~~~~~~~~~~~Local vars~~~~~~~~~~~~~~~~~~~~~*/
+    int ClientSocket = *(int*)ClientSocketPtr;
+    char stringBuffer[BUFFERSIZE];
+    bzero(stringBuffer, BUFFERSIZE);
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    fflush(stdout);
+    if( (read(ClientSocket, stringBuffer, sizeof(stringBuffer))) < 0) //If read message fails
+    {
+        printf("Failed to read message from client\n");
+    }
+    else //Else parse message and do something.
+    {
+        //stringBuffer[msgSize + 1] = '\0';
+        printf("Received message: %s\n", stringBuffer);
+        ParseClientMessage(stringBuffer, ClientSocket);
+    }
+    close(ClientSocket);
+    free(ClientSocketPtr);
+    pthread_exit(NULL);
 }
 
 
@@ -111,7 +217,47 @@ void HandleClientRequests(void* ClientSocketPtr)
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 void ParseClientMessage(char* clientMessage, int ClientSocket)
 {
+    /*~~~~~~~~~~~~~~~~~~~~~Local vars~~~~~~~~~~~~~~~~~~~~~*/
+    int i = 0;
+    char string[BUFFERSIZE]; //String to send back to client
+    char token[BUFFERSIZE];  //Token to use for echo reply
+    string[0] = '\0';
+    const int NUMLOADAVG = 3; //Number of load averages queries
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
+
+    /*~~~~~~~~~~~~~~~~~~~~~Load avg response~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    if(strcmp(clientMessage, "<loadavg/>") == 0)
+    {
+        double loadavg[NUMLOADAVG];
+        getloadavg(loadavg, NUMLOADAVG); //Get load average and put in double array
+
+        //Begin string format:
+        strcat(string, "<replyLoadAvg>");
+        for(i = 0; i < NUMLOADAVG; i++)
+        {
+            char tempAvg[BUFFERSIZE];
+            sprintf(tempAvg, "%lf:", loadavg[i]);
+            strcat(string, tempAvg);              //Append string to string we return to client
+        }
+        strcat(string, "</replyLoadAvg>\0"); //End of string
+    }
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+        /*~~~~~~~~~~~~~~~~~~~~~Echo response~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    else if ((XMLParser("<echo>", "</echo>", clientMessage, token, sizeof(token))) == 1)
+    {
+        //Set return echo string
+        strcat(string, "<reply>");
+        strcat(string, token);
+        strcat(string, "</reply>\0");
+    }
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    else //Else we have an invalid format
+        strcat(string, "<error>unknown format</error>\0");
+    printf("Sending back %s\n\n", string);
+    if( (send(ClientSocket, (void *) string, strlen(string), 0)) < 0) //Send string back to client.
+        printf("Failed to send\n");
 }
 
 
