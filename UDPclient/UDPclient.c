8c8ea1d7b04b598841a7f7aa31f04356e146cc10
christopher <ccs19@students.uwf.edu>
2015-02-02 23:24:10 -0600

    Added some necessary files and CMakeLists for each project sub directory. When you're using CLion, you can select which item to build from the drop down menu on the toolbar.

---
+++ UDPclient/UDPclient.c
@@ -0,0 +1,75 @@
+#include "UDPclient.h"
+
+
+
+/*
+ * UDPclient.c
+ * Systems and Networks II
+ * Project 2
+ *
+ */
+
+/*
+ * Creates a datagram socket and connects to a server.
+ *
+ * serverName - the ip address or hostname of the server given as a string
+ * serverPort - the port number of the server
+ * dest       - the server's address information; the structure will be initialized with information
+ *              on the server (like port, address, and family) in this function call
+ *
+ * return value - the socket identifier or a negative number indicating the error if a connection could not be established
+ */
+int createSocket(char * serverName, int serverPort, struct sockaddr_in * dest)
+{
+    return 0;
+}
+/*
+ * Sends a request for service to the server. This is an asynchronous call to the server,
+ * so do not wait for a reply in this function.
+ *
+ * sockFD  - the socket identifier
+ * request - the request to be sent encoded as a string
+ * dest    - the server's address information
+ *
+ * return   - 0, if no error; otherwise, a negative number indicating the error
+ */
+int sendRequest(int sockFD, char * request, struct sockaddr_in * dest)
+{
+    return 0;
+}
+
+/*
+ * Receives the server's response formatted as an XML text string.
+ *
+ * sockfd    - the socket identifier
+ * response  - the server's response as an XML formatted string to be filled in by this function into the specified string array
+ *
+ * return   - 0, if no error; otherwise, a negative number indicating the error
+ */
+int receiveResponse(int sockFD, char * response)
+{
+    return 0;
+}
+
+/*
+ * Prints the response to the screen in a formatted way.
+ *
+ * response - the server's response as an XML formatted string
+ *
+ */
+void printResponse(char * response)
+{
+
+}
+
+/*
+ * Closes the specified socket
+ *
+ * sockFD - the ID of the socket to be closed
+ *
+ * return - 0, if no error; otherwise, a negative number indicating the error
+ */
+int closeSocket(int sockFD)
+{
+    return 0;
+}
