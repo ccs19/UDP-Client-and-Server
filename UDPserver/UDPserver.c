e194fd70d8c2d863805b6d2b228c8830623275ec
christopher <ccs19@students.uwf.edu>
2015-02-03 00:06:44 -0600

    Added prototypes.
    From the look of the project description, we will need identical functions.

--- UDPserver/UDPserver.c
+++ UDPserver/UDPserver.c
@@ -1,11 +1,178 @@
 
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-
+#include "UDPserver.h"
 
 
 int main()
 {
-
-
     return 0;
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*  FUNCTION:   OpenSocket
+    Opens the listening socket and resolves the host name.
+    @param  port           -- The port number to bind the listen socket
+    @return                -- void
+ */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void OpenSocket(int port)
+{
+
+}
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*  FUNCTION:   InitAddressStruct
+    Initializes the ServerAddress structure with the IP, port, and protocol type.
+    @param  port           --  Port number to open
+    @return                --  void
+ */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void InitAddressStruct(int port)
+{
+
+}
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*  FUNCTION:   DisplayInfo
+    Displays the connection info of the server.
+    @return           --    void
+ */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void DisplayInfo()
+{
+
+}
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*  FUNCTION:   BindSocketAndListen
+    Binds the server socket and listens on that socket
+    @return           --    void
+ */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void BindSocketAndListen()
+{
+
+}
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*  FUNCTION:   AcceptConnections
+    Accepts the client connection and creates a new detached thread for the client.
+    @return           --    void
+ */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void AcceptConnections()
+{
+
+}
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*  FUNCTION:   ExitOnError
+    Prints a message to stdout and exits
+    @param  errorMessage           -- Error message to be printed
+    @return                        -- void
+ */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void ExitOnError(char* errorMessage)
+{
+    printf("%s\n", errorMessage);
+    exit(1);
+}
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*  FUNCTION:   HandleClientRequests
+    Depending on the string received from the client, we either print that it failed or parse
+    the message, then close the socket and free the thread.
+    @param  ClientSocketPtr -- A pointer to the client socket typecasted to a void*
+    @return           -- void
+ */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void HandleClientRequests(void* ClientSocketPtr)
+{
+
+}
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*  FUNCTION:   ParseClientMessage
+    Handles the message for the client and sends a message back to the client
+    @param  clientMessage        -- Pointer to message received by client
+    @param  ClientSocket         -- The socket to the client
+    @return                      -- void
+ */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void ParseClientMessage(char* clientMessage, int ClientSocket)
+{
+
+}
+
+
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*  FUNCTION: XMLParser
+    Parses an XML value and returns a token.
+    @param begin           --      The expected beginning of an XML expression
+    @param end             --      The expected ending of an XML expression
+    @param token           --      The token extracted from the expression
+    @param clientMessage   --      Message to parse
+    @param length          --      Size of token
+    @return                --      1 on success, 0 on failure, -1 if token is too large to fit
+ */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+int XMLParser(  const char* beginXml,
+        const char* endXml,
+        char* clientMessage,
+        char* token,
+        int tokenSize)
+{
+    //~~~~~~~~~~~~~Local vars ~~~~~~~~~~~~~~~~~~~~~~~//
+    char tempString[strlen(clientMessage)];
+    char *delimiter = NULL;
+    int returnVal = 0;
+    int i = 0;
+    int foundDelimiter = 0;
+    int beginXmlLength = strlen(beginXml);
+    int endXmlLength = strlen(endXml);
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+    token[0] = '\0';
+    memcpy(tempString, clientMessage, beginXmlLength); //Copy first part of clientMessage into temp for comparison.
+    tempString[beginXmlLength] = '\0';
+    if(strcmp(tempString, beginXml) == 0 ) //If beginXml is found
+    {
+        memcpy(tempString, clientMessage, strlen(clientMessage)); //Copy entire clientMessage
+        for(i = 1; i < strlen(clientMessage); i++) //Check for valid delimiter here
+        {
+            if(tempString[i] == '<')
+            {
+                delimiter = tempString+i;//Potential valid delimiter found. Point delimiter ptr to location.
+                foundDelimiter = 1;
+                break;
+            }
+        }
+        if(foundDelimiter)
+        {
+            delimiter[endXmlLength] = '\0';//Set end of delimiter to null
+            if (strcmp(delimiter, endXml) != 0) //If invalid delimiter
+                returnVal = 0;
+            else {
+                returnVal = 1;//Set valid return
+                char *tempToken = clientMessage + (strlen(beginXml)); //Set temporary token to end of starting delimiter
+                strtok(tempToken, "<");
+                if (strlen(tempToken) > tokenSize) returnVal = -1;              //If token is too large, return -1
+                else if (strcmp(tempToken, endXml) == 0) token[0] = '\0';       //Else if empty token found
+                else strcat(token, tempToken);                                  //Else put extracted token in variable
+            }
+        }
+    }
+    return returnVal;
 }
